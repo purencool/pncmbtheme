cbacaaccedf64a80b94170e89e69da571d4c0510
purencool <purencool@gmail.com>
2015-01-01 19:22:34 +1100

    Added gallery theming and moved jquery image directory

--- js/script.js
+++ js/script.js
@@ -7,11 +7,30 @@
           var clostestTag = $( this ).parent().parent();
           //$(clostestTag).find(".bg-group").toggle();
           $("ul > li .colorbox-inline").colorbox({
-              width: "70%",
+              width: "100%",
               inline:true,
               href: $(clostestTag).find(".bg-group")
           });
         });
+
+        $('ul > li .colorbox-inline').hover(
+          function(){
+            $(this).find('.overlay').stop().fadeTo(500, 0.7);
+          },
+          function(){
+            $(this).find('.overlay').stop().fadeTo(500, 0);
+         });
+
+         var oldSize = parseFloat($("ul > li .colorbox-inline a").css('font-size'));
+         var newSize = oldSize  * 2;
+
+         $('ul > li .colorbox-inline').hover(
+           function(){
+             $(this).find('.title').css( "display", "block" );
+           },
+           function(){
+             $(this).find('.title').css( "display", "none" );
+          });
       });
     }
   };
